f3a2399ea26f882bb3a1ff7780b1bc568760c09a
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-06-09 19:44:33 +0900

    TextField周辺のデザインを追加

--- lib/presentation/components/text_field_widget.dart
+++ lib/presentation/components/text_field_widget.dart
@@ -1,43 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:social_mobile/utils/theme/extension/theme_extension.dart';
 
-class TextFieldWidget extends StatelessWidget {
+class TextFieldWidget extends HookWidget {
   const TextFieldWidget({
     super.key,
     required this.labelText,
+    required this.helperText,
+    required this.maxLength,
+    this.validator,
+    this.onFieldSubmitted,
+    required this.controller,
   });
   final String labelText;
+  final String helperText;
+  final int maxLength;
+  final String? Function(String?)? validator;
+  final void Function(String)? onFieldSubmitted;
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.customThemeExtension.textTheme;
     final colorTheme = theme.customThemeExtension.colorTheme;
+    final inputTextLength = useState(0);
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: TextFormField(
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: TextFormField(
+        validator: validator,
+        controller: controller,
+        onChanged: (value) {
+          inputTextLength.value = value.length;
+        },
+        onFieldSubmitted: onFieldSubmitted,
+        style: textTheme.body.copyWith(
+          color: colorTheme.neutral[10],
+        ),
+        maxLength: maxLength,
+        cursorColor: colorTheme.primary[40],
+        cursorErrorColor: colorTheme.error[40],
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: textTheme.body.copyWith(
+            color: colorTheme.neutral[30],
+          ),
+          counter: Text(
+            '${inputTextLength.value}/$maxLength',
             style: textTheme.body.copyWith(
               color: colorTheme.neutral[10],
             ),
-            decoration: InputDecoration(
-              labelText: labelText,
-              labelStyle: textTheme.body.copyWith(
-                color: colorTheme.neutral[30],
-              ),
-              contentPadding: const EdgeInsets.all(16),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: colorTheme.neutral[50]!),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: colorTheme.neutral[50]!),
-              ),
+          ),
+          helperText: helperText,
+          helperStyle: textTheme.body.copyWith(
+            color: colorTheme.neutral[10],
+          ),
+          contentPadding: const EdgeInsets.all(16),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorTheme.neutral[50]!),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: colorTheme.primary[40]!,
+              width: 2,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorTheme.error[40]!),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: colorTheme.error[40]!,
+              width: 2,
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
