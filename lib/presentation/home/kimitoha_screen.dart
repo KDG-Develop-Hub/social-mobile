2278b372112f85aa8f5beabea119d5f4939f78c6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-11 23:55:25 +0900

    君とはページ作成

---
+++ lib/presentation/home/kimitoha_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KimitohaScreen extends StatelessWidget {
+  const KimitohaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('君とは'),
+      ),
+    );
+  }
+}
