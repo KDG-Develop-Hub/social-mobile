f3597630e224550e9299f9017961b0074a433124
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-11 23:56:07 +0900

    知り合いページ作成

---
+++ lib/presentation/home/siriai_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SiriaiScreen extends StatelessWidget {
+  const SiriaiScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('知り合い'),
+      ),
+    );
+  }
+}
