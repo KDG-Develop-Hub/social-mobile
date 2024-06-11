a2a49816f5a4f59ad690d65c3c460f1605679f43
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-11 23:51:55 +0900

    ボトムナビゲーターバーに使用するアイコン画像をGenでdartコードにコンバート

---
+++ lib/utils/gen/assets.gen.dart
@@ -0,0 +1,62 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/apude_icon.svg
+  String get apudeIcon => 'assets/images/apude_icon.svg';
+
+  /// File path: assets/images/kimitoha_icon.svg
+  String get kimitohaIcon => 'assets/images/kimitoha_icon.svg';
+
+  /// File path: assets/images/mitukeru_icon.svg
+  String get mitukeruIcon => 'assets/images/mitukeru_icon.svg';
+
+  /// File path: assets/images/selected_apude_icon.svg
+  String get selectedApudeIcon => 'assets/images/selected_apude_icon.svg';
+
+  /// File path: assets/images/selected_kimitoha_icon.svg
+  String get selectedKimitohaIcon => 'assets/images/selected_kimitoha_icon.svg';
+
+  /// File path: assets/images/selected_mitukeru_icon.svg
+  String get selectedMitukeruIcon => 'assets/images/selected_mitukeru_icon.svg';
+
+  /// File path: assets/images/selected_sette_icon.svg
+  String get selectedSetteIcon => 'assets/images/selected_sette_icon.svg';
+
+  /// File path: assets/images/selected_siriai_icon.svg
+  String get selectedSiriaiIcon => 'assets/images/selected_siriai_icon.svg';
+
+  /// File path: assets/images/sette_icon.svg
+  String get setteIcon => 'assets/images/sette_icon.svg';
+
+  /// File path: assets/images/siriai_icon.svg
+  String get siriaiIcon => 'assets/images/siriai_icon.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        apudeIcon,
+        kimitohaIcon,
+        mitukeruIcon,
+        selectedApudeIcon,
+        selectedKimitohaIcon,
+        selectedMitukeruIcon,
+        selectedSetteIcon,
+        selectedSiriaiIcon,
+        setteIcon,
+        siriaiIcon
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
