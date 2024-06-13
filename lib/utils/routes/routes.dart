d2e6ec6b84bc21bcd75412bd12941b2afb444eaa
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-13 20:50:07 +0900

    BottomNavigationsクラスのコードを別ファイルに分けた

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:social_mobile/presentation/components/bottom_navigation/bottom_navigation.dart';
 import 'package:social_mobile/presentation/home/apude_screen.dart';
 import 'package:social_mobile/presentation/home/kimitoha_screen.dart';
 import 'package:social_mobile/presentation/home/mitukeru_screen.dart';
 import 'package:social_mobile/presentation/home/sette_screen.dart';
 import 'package:social_mobile/presentation/home/siriai_screen.dart';
-import 'package:social_mobile/presentation/home/view_model/bottom_navigation_notifier.dart';
-import 'package:social_mobile/utils/gen/assets.gen.dart';
-import 'package:social_mobile/utils/theme/extension/theme_extension.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/apude',
@@ -54,72 +50,3 @@ final appRouter = GoRouter(
     ),
   ],
 );
-
-class BottomNavigation extends ConsumerWidget {
-  const BottomNavigation({super.key, required this.child});
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final theme = Theme.of(context);
-    final colorTheme = theme.customThemeExtension.colorTheme;
-    final index = ref.watch(bottomNavigationNotifierProvider);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: colorTheme.neutral[94],
-        selectedIndex: index,
-        indicatorColor: const Color.fromRGBO(209, 229, 184, 1),
-        onDestinationSelected: (int newIndex) {
-          ref
-              .read(bottomNavigationNotifierProvider.notifier)
-              .setIndex(newIndex);
-          switch (newIndex) {
-            case 0:
-              context.go('/apude');
-            case 1:
-              context.go('/mitukeru');
-            case 2:
-              context.go('/kimitoha');
-            case 3:
-              context.go('/siriai');
-            case 4:
-              context.go('/sette');
-          }
-        },
-        destinations: [
-          NavigationDestination(
-            icon: SvgPicture.asset(
-              Assets.images.apudeIcon,
-            ),
-            label: 'アプデ',
-          ),
-          NavigationDestination(
-            icon: SvgPicture.asset(
-              Assets.images.mitukeruIcon,
-            ),
-            label: '見つける',
-          ),
-          NavigationDestination(
-            icon: SvgPicture.asset(
-              Assets.images.kimitohaIcon,
-            ),
-            label: '君とは',
-          ),
-          NavigationDestination(
-            icon: SvgPicture.asset(
-              Assets.images.siriaiIcon,
-            ),
-            label: '知り合い',
-          ),
-          NavigationDestination(
-            icon: SvgPicture.asset(
-              Assets.images.setteIcon,
-            ),
-            label: 'せってー',
-          ),
-        ],
-      ),
-      body: child,
-    );
-  }
-}
