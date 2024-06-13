213490505bb348c0772d610b375427de12a86fc6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-13 13:48:44 +0900

    ルートのファイル場所を変更

--- lib/presentation/social_mobile.dart
+++ lib/presentation/social_mobile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:social_mobile/presentation/bottom_navigation.dart';
+import 'package:social_mobile/utils/routes/routes.dart';
 import 'package:social_mobile/utils/theme/theme.dart';
 
 class SocialMobile extends HookConsumerWidget {
