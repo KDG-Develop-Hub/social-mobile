6440e5584b899f8236696ef84df61b56b2fd731d
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-11 00:56:31 +0900

    デザイン修正

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -21,72 +21,70 @@ class ProfileScreen extends StatelessWidget {
   }
 
   Widget buildProfileHeader(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: NetworkImage(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR36cWx-s4D8DVd14R6CcqEa5WMed68mVEQ5w&s',
-                    ),
-                    fit: BoxFit.cover,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR36cWx-s4D8DVd14R6CcqEa5WMed68mVEQ5w&s',
                   ),
-                  borderRadius: BorderRadius.circular(16),
+                  fit: BoxFit.cover,
                 ),
-                height: 64,
-                width: 64,
+                borderRadius: BorderRadius.circular(16),
               ),
-              const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '大村健心',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  Text(
-                    'imeankenshin',
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Theme.of(context).colorScheme.primary,
-              textStyle: Theme.of(context).textTheme.labelMedium,
-              side: BorderSide(
-                color: Theme.of(context).colorScheme.outline,
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              height: 64,
+              width: 64,
             ),
-            onPressed: () {},
-            child: const Text('プロフィールをいじる'),
+            const SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '大村健心',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  'imeankenshin',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Theme.of(context).colorScheme.primary,
+            textStyle: Theme.of(context).textTheme.labelMedium,
+            side: BorderSide(
+              color: Theme.of(context).colorScheme.outline,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
           ),
-          const SizedBox(height: 16),
-          Text(
-            '小さな頭に大きな夢を持っています。エンジニアを目指す現役高校生じゃーい！',
-            style: Theme.of(context).textTheme.bodyLarge,
-            softWrap: true,
-            maxLines: 2,
-          ),
-        ],
-      ),
+          onPressed: () {},
+          child: const Text('プロフィールをいじる'),
+        ),
+        const SizedBox(height: 16),
+        Text(
+          '小さな頭に大きな夢を持っています。エンジニアを目指す現役高校生じゃーい！',
+          style: Theme.of(context).textTheme.bodyLarge,
+          softWrap: true,
+          maxLines: 2,
+        ),
+      ],
     );
   }
 
   PreferredSizeWidget? buildTabs(BuildContext context) {
     return TabBar(
-      padding: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.only(top: 16, right: 20, left: 20),
       indicatorColor: Theme.of(context).colorScheme.primary,
-      indicatorPadding: const EdgeInsets.symmetric(horizontal: 16),
+      indicatorPadding: const EdgeInsets.symmetric(horizontal: 8),
       tabs: <Widget>[
         tab(context: context, tabName: 'つぶやき'),
         tab(context: context, tabName: '返信'),
