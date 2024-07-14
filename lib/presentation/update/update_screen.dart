e014a7da277ceb7769dea3731cf20bae59106f4a
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-07-15 00:34:18 +0900

    アップデート画面でモックのPost一覧を表示

--- lib/presentation/update/update_screen.dart
+++ lib/presentation/update/update_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_mobile/i18n/strings.g.dart';
-import 'package:social_mobile/presentation/provider/locale_service.dart';
+import 'package:social_mobile/presentation/components/loading.dart';
+import 'package:social_mobile/presentation/components/post_card.dart';
 import 'package:social_mobile/presentation/update/provider/update_notifier.dart';
 
 class UpdateScreen extends ConsumerWidget {
@@ -9,63 +10,35 @@ class UpdateScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(updateNotifierProvider);
-    final counterNotifier = ref.read(updateNotifierProvider.notifier);
-    final localeState = ref.watch(localeServiceProvider);
+    final state = ref.watch(updateNotifierProvider);
     final translations = Translations.of(context);
-    final socialMobilePageI18n = translations.socialMobilePage;
-    final settingLanguageTitle = socialMobilePageI18n.changeLanguage;
+    final textStyle = Theme.of(context).textTheme;
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              socialMobilePageI18n.pushExplanation,
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(socialMobilePageI18n.languageExplanation),
-            const Padding(padding: EdgeInsets.all(30)),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  // NOTE change language
-                  if (localeState == AppLocale.en) {
-                    ref
-                        .read(localeServiceProvider.notifier)
-                        .changeLocale(AppLocale.ja);
-                  } else {
-                    ref
-                        .read(localeServiceProvider.notifier)
-                        .changeLocale(AppLocale.en);
-                  }
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(settingLanguageTitle),
-                    const Spacer(),
-                    const Icon(Icons.language),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        title: Text(
+          translations.navigationBar.update,
+          style: textStyle.headlineMedium,
         ),
+        centerTitle: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counterNotifier.increment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: switch (state) {
+        AsyncData(:final value) => ListView.builder(
+            itemCount: value.length,
+            itemBuilder: (_, index) => PostCard(
+              post: value[index],
+              onPostTap: () {},
+            ),
+          ),
+        AsyncLoading() => const Loading(),
+        AsyncError(:final error, :final stackTrace) => Center(
+            child: Text(
+              '$error\n$stackTrace',
+              style: textStyle.labelMedium,
+            ),
+          ),
+        _ => const Loading(),
+      },
     );
   }
 }
