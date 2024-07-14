ff6edad443d8392d01261c4d4375c9048ea42dcd
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-07-14 23:36:56 +0900

    モックしたUserリポジトリを新しいエンティティ構造に合わせて修正する

---
+++ lib/infrastructure/user/mocks/mock_user_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:social_mobile/domain/role/role.dart';
+import 'package:social_mobile/domain/user/user.dart';
+import 'package:social_mobile/domain/user/user_repository.dart';
+import 'package:social_mobile/utils/helpers/random_string.dart';
+
+final mockUserRepositoryImplProvider = Provider(
+  (ref) => MockUserRepositoryImpl(),
+);
+
+class MockUserRepositoryImpl implements UserRepository {
+  final user = User(
+    id: '1',
+    name: 'Omura Kenshin',
+    imageUrl: 'https://picsum.photos/200',
+    bio: generateRandomString(),
+    createdAt: DateTime.now(),
+    roles: const [
+      Role(
+        id: '1',
+        name: 'Admin',
+      ),
+    ],
+  );
+
+  @override
+  Future<User> fetchUser() async {
+    await Future<void>.delayed(const Duration(seconds: 3));
+    return user;
+  }
+}
