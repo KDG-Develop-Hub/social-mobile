f24b5654c053cf7667c1e97693b3359930b342ac
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-07-22 20:27:30 +0900

    SVGアイコンの共通化

---
+++ lib/presentation/components/post_icon_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PostIconButton extends StatelessWidget {
+  const PostIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+  });
+  final String icon;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 20,
+      height: 20,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: onPressed,
+        icon: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
