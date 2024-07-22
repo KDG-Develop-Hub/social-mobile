ced23bf84939c7f5b3504e5c9a52edf3c69664dd
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-07-22 20:25:19 +0900

    アイコン素材の追加

--- lib/utils/gen/assets.gen.dart
+++ lib/utils/gen/assets.gen.dart
@@ -10,11 +10,26 @@
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
+  /// File path: assets/icons/bookmark.svg
+  String get bookmark => 'assets/icons/bookmark.svg';
+
+  /// File path: assets/icons/bookmarked.svg
+  String get bookmarked => 'assets/icons/bookmarked.svg';
+
+  /// File path: assets/icons/comment.svg
+  String get comment => 'assets/icons/comment.svg';
+
+  /// File path: assets/icons/flag.svg
+  String get flag => 'assets/icons/flag.svg';
+
   /// File path: assets/icons/friends_icon.svg
   String get friendsIcon => 'assets/icons/friends_icon.svg';
 
   /// File path: assets/icons/profile_icon.svg
   String get profileIcon => 'assets/icons/profile_icon.svg';
+
+  /// File path: assets/icons/reaction.svg
+  String get reaction => 'assets/icons/reaction.svg';
 
   /// File path: assets/icons/search_icon.svg
   String get searchIcon => 'assets/icons/search_icon.svg';
@@ -22,12 +37,30 @@ class $AssetsIconsGen {
   /// File path: assets/icons/settings_icon.svg
   String get settingsIcon => 'assets/icons/settings_icon.svg';
 
+  /// File path: assets/icons/share.svg
+  String get share => 'assets/icons/share.svg';
+
+  /// File path: assets/icons/trash.svg
+  String get trash => 'assets/icons/trash.svg';
+
   /// File path: assets/icons/update_icon.svg
   String get updateIcon => 'assets/icons/update_icon.svg';
 
   /// List of all assets
-  List<String> get values =>
-      [friendsIcon, profileIcon, searchIcon, settingsIcon, updateIcon];
+  List<String> get values => [
+        bookmark,
+        bookmarked,
+        comment,
+        flag,
+        friendsIcon,
+        profileIcon,
+        reaction,
+        searchIcon,
+        settingsIcon,
+        share,
+        trash,
+        updateIcon
+      ];
 }
 
 class Assets {
