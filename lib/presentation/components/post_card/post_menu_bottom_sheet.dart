3604361a62476b65c0f2a7a3158b423c2522e199
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-07-24 13:30:10 +0900

    Postコンポーネントのメニューボタンをポップアップからボトムシートに変更

---
+++ lib/presentation/components/post_card/post_menu_bottom_sheet.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:social_mobile/i18n/strings.g.dart';
+import 'package:social_mobile/presentation/components/post_card/post_menu_button.dart';
+import 'package:social_mobile/utils/gen/assets.gen.dart';
+
+enum PostMenuOption {
+  edit,
+  delete,
+  reaction,
+  comment,
+  report,
+  share,
+}
+
+class PostMenuBottomSheet extends StatelessWidget {
+  const PostMenuBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorTheme = Theme.of(context).colorScheme;
+    final translations = Translations.of(context).updateScreen.post.popUpMenu;
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: colorTheme.surface,
+        borderRadius: BorderRadius.circular(28),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 32,
+            height: 2,
+            decoration: BoxDecoration(
+              color: colorTheme.onSurfaceVariant,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          const Gap(14),
+          Column(
+            children: [
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.comment,
+                label: translations.comment,
+              ),
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.reaction,
+                label: translations.reaction,
+              ),
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.bookmark,
+                label: translations.bookMark,
+              ),
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.share,
+                label: translations.share,
+              ),
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.trash,
+                label: translations.delete,
+                isRedText: true,
+              ),
+              PostMenuButton(
+                onPressed: () {},
+                icon: Assets.icons.flag,
+                label: translations.report,
+                isRedText: true,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
