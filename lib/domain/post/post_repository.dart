82daaa5bbbfa81f04453debf3627c5feb7da1f26
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-09-03 02:47:21 +0900

    PostRepositoryにeditReactionメソッドを実装

--- lib/domain/post/post_repository.dart
+++ lib/domain/post/post_repository.dart
@@ -1,6 +1,11 @@
 import 'package:social_mobile/domain/post/post.dart';
+import 'package:social_mobile/domain/reaction/reaction.dart';
 
 // ignore: one_member_abstracts
 abstract interface class PostRepository {
   Future<List<Post>> fetchPosts();
+  Future<List<Post>> editReaction({
+    required bool hasReact, /// リアクションを追加するか削除するか
+    required Reaction reaction,
+  });
 }
