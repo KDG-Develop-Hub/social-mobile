692102c611361f11bb8577005d0fed8fa3277e0c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-09-03 02:40:17 +0900

    認証データの状態を持つ MockProviderの実装

---
+++ lib/presentation/provider/user_authentication.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_authentication.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userAuthenticationHash() =>
+    r'b8f8de55bf37fde954a30894bd8deb38984d9a6e';
+
+/// See also [userAuthentication].
+@ProviderFor(userAuthentication)
+final userAuthenticationProvider = Provider<User>.internal(
+  userAuthentication,
+  name: r'userAuthenticationProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userAuthenticationHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserAuthenticationRef = ProviderRef<User>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
