d13ee16638a3644a2d60dafad06aff786cf3d4d2
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-09-21 15:04:56 +0900

    ポストのボトムシートからリアクション追加

--- lib/presentation/components/post_card/post_card.dart
+++ lib/presentation/components/post_card/post_card.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:keyboard_emoji_picker/keyboard_emoji_picker.dart';
 import 'package:social_mobile/domain/post/post.dart';
@@ -30,11 +31,39 @@ class PostCard extends HookConsumerWidget {
     final isBookMarked = useState(false);
     final emojiShowing = useState(false);
 
+    Future<void> pickEmoji() async {
+      final hasEmojiKeyboard =
+          await KeyboardEmojiPicker().checkHasEmojiKeyboard();
+      if (hasEmojiKeyboard && emojiShowing.value) {
+        await KeyboardEmojiPicker().closeEmojiKeyboard();
+        emojiShowing.value = false;
+      } else {
+        emojiShowing.value = true;
+        final selectedEmoji = await KeyboardEmojiPicker().pickEmoji();
+        if (selectedEmoji != null) {
+          final reaction = Reaction(
+            id: selectedEmoji,
+            postId: post.id,
+            userIds: [],
+            emoji: selectedEmoji,
+          );
+          onReactionTap(reaction, hasReact: true);
+        }
+        emojiShowing.value = false;
+      }
+    }
+
     Future<void> showPostMenuBottomSheet() async {
       await showModalBottomSheet<void>(
         context: context,
         useRootNavigator: true,
-        builder: (_) => PostMenuBottomSheet(post: post),
+        builder: (_) => PostMenuBottomSheet(
+          post: post,
+          onReactionTap: () async {
+            context.pop();
+            await pickEmoji();
+          },
+        ),
       );
     }
 
@@ -126,26 +155,7 @@ class PostCard extends HookConsumerWidget {
                         PostIconButton(
                           icon: Assets.icons.reaction,
                           onPressed: () async {
-                            final hasEmojiKeyboard = await KeyboardEmojiPicker()
-                                .checkHasEmojiKeyboard();
-                            if (hasEmojiKeyboard && emojiShowing.value) {
-                              await KeyboardEmojiPicker().closeEmojiKeyboard();
-                              emojiShowing.value = false;
-                            } else {
-                              emojiShowing.value = true;
-                              final selectedEmoji =
-                                  await KeyboardEmojiPicker().pickEmoji();
-                              if (selectedEmoji != null) {
-                                final reaction = Reaction(
-                                  id: selectedEmoji,
-                                  postId: post.id,
-                                  userIds: [],
-                                  emoji: selectedEmoji,
-                                );
-                                onReactionTap(reaction, hasReact: true);
-                              }
-                              emojiShowing.value = false;
-                            }
+                            await pickEmoji();
                           },
                         ),
                         PostIconButton(
