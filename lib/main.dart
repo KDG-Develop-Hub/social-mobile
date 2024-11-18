f5e8b7bdc80a730fabe26d3cede4a378198c68ba
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 22:09:46 +0900

    firebaseの環境変数周りを修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:social_mobile/firebase_options.dart';
 import 'package:social_mobile/i18n/strings.g.dart';
+import 'package:social_mobile/infrastructure/firebase/firebase_options_dev.dart'
+    as dev;
+import 'package:social_mobile/infrastructure/firebase/firebase_options_prod.dart'
+    as prod;
 import 'package:social_mobile/presentation/provider/shared_preferences_service.dart';
 import 'package:social_mobile/presentation/social_mobile.dart';
 
@@ -11,9 +14,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   LocaleSettings.useDeviceLocale();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  final options = const bool.fromEnvironment('flavor')
+      ? prod.DefaultFirebaseOptions.currentPlatform
+      : dev.DefaultFirebaseOptions.currentPlatform;
+
+  await Firebase.initializeApp(options: options);
   runApp(
     ProviderScope(
       overrides: [
