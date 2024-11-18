2b8679f094a0ea035e7bf4cd0faae2642f766a8c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 23:25:18 +0900

    投稿画面用のNotifierを追加

---
+++ lib/presentation/post/post_screen_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_screen_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$postScreenNotifierHash() =>
+    r'687e3d178329783f47b9c21aa6ffd660ad807082';
+
+/// See also [PostScreenNotifier].
+@ProviderFor(PostScreenNotifier)
+final postScreenNotifierProvider =
+    AutoDisposeNotifierProvider<PostScreenNotifier, List<File>?>.internal(
+  PostScreenNotifier.new,
+  name: r'postScreenNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$postScreenNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PostScreenNotifier = AutoDisposeNotifier<List<File>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
