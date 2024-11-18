d0230ccdfccb29f83cc2eeaaf068efa25fd7d165
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 22:13:51 +0900

    Snackbarを管理するクラス実装

---
+++ lib/utils/helpers/scaffold_messenger.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'scaffold_messenger.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$scaffoldMessengerKeyHash() =>
+    r'f809f6dcf7a4f92ce76d0fa47e0bb51c45039b72';
+
+/// See also [scaffoldMessengerKey].
+@ProviderFor(scaffoldMessengerKey)
+final scaffoldMessengerKeyProvider =
+    Provider<GlobalKey<ScaffoldMessengerState>>.internal(
+  scaffoldMessengerKey,
+  name: r'scaffoldMessengerKeyProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$scaffoldMessengerKeyHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ScaffoldMessengerKeyRef
+    = ProviderRef<GlobalKey<ScaffoldMessengerState>>;
+String _$scaffoldMessengerHash() => r'f88f1fd1267f8b46309a5d3ac1007b0434ee2b2e';
+
+/// See also [ScaffoldMessenger].
+@ProviderFor(ScaffoldMessenger)
+final scaffoldMessengerProvider =
+    AutoDisposeNotifierProvider<ScaffoldMessenger, void>.internal(
+  ScaffoldMessenger.new,
+  name: r'scaffoldMessengerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$scaffoldMessengerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ScaffoldMessenger = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
