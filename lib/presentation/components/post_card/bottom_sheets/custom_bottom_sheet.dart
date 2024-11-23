56cb2b1ad714a5a4a09916b065f438ee724f9c0b
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-11-23 21:16:37 +0900

    feat: ボトムシートの共通化する部分を増やした

--- lib/presentation/components/post_card/bottom_sheets/custom_bottom_sheet.dart
+++ lib/presentation/components/post_card/bottom_sheets/custom_bottom_sheet.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:social_mobile/domain/post/post.dart';
+import 'package:social_mobile/i18n/strings.g.dart';
+import 'package:social_mobile/presentation/components/post_card/contents_only_post_card.dart';
 
 class CustomBottomSheet extends StatelessWidget {
-  const CustomBottomSheet({super.key, required this.content});
+  const CustomBottomSheet({
+    super.key,
+    required this.post,
+    required this.iconPath,
+    required this.title,
+    required this.buttonText,
+    required this.onPressedButton,
+    this.content,
+    this.explanation = '',
+    this.buttomPadding = 0,
+  });
 
-  final Widget content;
+  final Post post;
+  final String iconPath;
+  final String title;
+  final String buttonText;
+  final void Function()? onPressedButton;
+  final Widget? content;
+  final String explanation;
+  final double buttomPadding;
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     final colorTheme = Theme.of(context).colorScheme;
+    final translations = Translations.of(context);
+    final homeScreenI18n = translations.homeScreen;
 
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
       decoration: BoxDecoration(
         color: colorTheme.surface,
         borderRadius: BorderRadius.circular(28),
@@ -29,7 +53,50 @@ class CustomBottomSheet extends StatelessWidget {
             ),
           ),
           const Gap(14),
-          content,
+          SvgPicture.asset(
+            iconPath,
+            height: 24,
+            width: 24,
+          ),
+          const Gap(16),
+          Text(
+            title,
+            style: textTheme.headlineSmall,
+          ),
+          const Gap(16),
+          if (explanation.isNotEmpty) ...[
+            Text(
+              explanation,
+              style: textTheme.bodyLarge,
+            ),
+            const Gap(16),
+          ],
+          ContentsOnlyPostCard(post: post),
+          if (content != null) ...[
+            const Gap(16),
+            content!,
+          ],
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Spacer(),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: colorTheme.primary,
+                ),
+                onPressed: () {},
+                child: Text(homeScreenI18n.bottomSheet.cancelButton),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: colorTheme.error,
+                ),
+                onPressed: onPressedButton,
+                child: Text(buttonText),
+              ),
+            ],
+          ),
+          Gap(buttomPadding),
         ],
       ),
     );
