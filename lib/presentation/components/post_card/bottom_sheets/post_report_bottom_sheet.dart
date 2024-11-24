7d85c59685195251a7ed28cdd0af7e3c917b0559
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-11-24 17:15:05 +0900

    feat: 多言語対応と、customBottomSheet変更に伴う修正、hooksを使ってラジオボタンが選択できるようにした

--- lib/presentation/components/post_card/bottom_sheets/post_report_bottom_sheet.dart
+++ lib/presentation/components/post_card/bottom_sheets/post_report_bottom_sheet.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:gap/gap.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:social_mobile/domain/post/post.dart';
+import 'package:social_mobile/i18n/strings.g.dart';
 import 'package:social_mobile/presentation/components/post_card/bottom_sheets/custom_bottom_sheet.dart';
-import 'package:social_mobile/presentation/components/post_card/simple_post_card.dart';
 import 'package:social_mobile/utils/gen/assets.gen.dart';
 
-class PostReportBottomSheet extends StatelessWidget {
+enum ReportReasons {
+  spam,
+  slander,
+  offensiveAndLewdContent,
+  misleadingInformation,
+  exposureOfPrivateInformation,
+}
+
+class PostReportBottomSheet extends HookWidget {
   const PostReportBottomSheet({super.key, required this.post});
 
   final Post post;
@@ -15,80 +21,57 @@ class PostReportBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    final colorTheme = Theme.of(context).colorScheme;
-    final reportReasons = [
-      'スパム',
-      '誹謗中傷',
-      '攻撃的、卑劣なコンテンツ',
-      '誤解を生む情報',
-      'プライベートな情報の提示',
-    ];
+    final translations = Translations.of(context);
+    final bottomSheetI18n = translations.bottomSheet;
+    final homeScreenI18n = translations.homeScreen;
+    final reportReasons = bottomSheetI18n.postReport.reportReasons;
+    final groupValue = useState<ReportReasons?>(null);
+    final reportReasonsMap = <ReportReasons, String>{
+      ReportReasons.spam: reportReasons.spam,
+      ReportReasons.slander: reportReasons.slander,
+      ReportReasons.offensiveAndLewdContent:
+          reportReasons.offensiveAndLewdContent,
+      ReportReasons.misleadingInformation: reportReasons.misleadingInformation,
+      ReportReasons.exposureOfPrivateInformation:
+          reportReasons.exposureOfPrivateInformation,
+    };
 
     return CustomBottomSheet(
-      content: Column(
-        children: [
-          SvgPicture.asset(
-            Assets.icons.flag,
-            height: 24,
-            width: 24,
-          ),
-          const Gap(16),
-          Text(
-            '通報する理由を教えてください。',
-            style: textTheme.headlineSmall,
-          ),
-          const Gap(16),
-          SimplePostCard(post: post),
-          const Gap(16),
-          SizedBox(
-            height: 200,
-            child: ListView.builder(
-              itemCount: reportReasons.length,
-              itemBuilder: (_, index) {
-                final reportReason = reportReasons[index];
-                return SizedBox(
+      iconPath: Assets.icons.flag,
+      title: bottomSheetI18n.postReport.title,
+      post: post,
+      content: SizedBox(
+        height: 200,
+        child: Column(
+          children: reportReasonsMap.entries
+              .map(
+                (entry) => SizedBox(
                   height: 40,
                   child: RadioListTile(
-                    visualDensity: const VisualDensity(horizontal: -4),
+                    visualDensity: const VisualDensity(
+                      horizontal: VisualDensity.minimumDensity,
+                    ),
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     title: Text(
-                      reportReason,
+                      entry.value,
                       style: textTheme.bodyMedium,
                     ),
-                    value: 0,
-                    groupValue: 0,
-                    onChanged: (value) {},
+                    value: entry.key,
+                    groupValue: groupValue.value,
+                    onChanged: (value) {
+                      if (value != null) {
+                        groupValue.value = value;
+                      }
+                    },
                   ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 24),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Spacer(),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: colorTheme.primary,
-                  ),
-                  onPressed: () {},
-                  child: const Text('やっぱやめる'),
                 ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: colorTheme.error,
-                  ),
-                  onPressed: () {
-                    context.pop();
-                  },
-                  child: const Text('通報する'),
-                ),
-              ],
-            ),
-          ),
-        ],
+              )
+              .toList(),
+        ),
       ),
+      buttonText: homeScreenI18n.post.popUpMenu.report,
+      onPressedButton: () {},
+      buttomPadding: 24,
     );
   }
 }
