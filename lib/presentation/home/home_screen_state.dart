a43439b363a0bc55017b68b9d021306e32831696
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-26 19:09:23 +0900

    ホームページの状態管理用モデル

---
+++ lib/presentation/home/home_screen_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:social_mobile/domain/post/post.dart';
+
+part 'home_screen_state.freezed.dart';
+
+@freezed
+abstract class HomeScreenState with _$HomeScreenState {
+  const factory HomeScreenState({
+    required List<Post> posts,
+    @Default(false) bool isLoadingMore,
+}) = _HomeScreenState;
+}
